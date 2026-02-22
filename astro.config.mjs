4eaf1d8288448c7eb2f180dad56684daf25fe7d2
Johana <johanaveas_26@outlook.com>
2026-02-22 01:55:06 -0300

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://JohanaMDev.github.io',
+  base : '/JohanaMDev.github.io/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
