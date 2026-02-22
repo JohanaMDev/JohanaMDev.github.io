30bc995e132dabfcc11315ef7bca5464155dcab3
Johana <johanaveas_26@outlook.com>
2026-02-22 01:46:48 -0300

    update indentity

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
+export const SITE_TITLE = 'Johana Veas | Java Developer';
+export const SITE_DESCRIPTION = 'Portfolio of Johana Veas, a Java Developer with experience in building scalable applications and a passion for clean code.';
+export const GENERATE_SLUG_FROM_TITLE = false;
 export const TRANSITION_API = true
