37378ba350bcc5baf787fbfd2f2a68de3c9ca5f2
Johana <johanaveas_26@outlook.com>
2026-02-22 02:19:44 -0300

    fix: corregir base path y autorizar scripts

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@
 
 export const SITE_TITLE = 'Johana Veas | Java Developer';
 export const SITE_DESCRIPTION = 'Portfolio of Johana Veas, a Java Developer with experience in building scalable applications and a passion for clean code.';
-export const GENERATE_SLUG_FROM_TITLE = false;
+export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true
